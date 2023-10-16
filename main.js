b5fffc222c2a1d2e103736906a3b17e862b507b7
OmololaBecca <folarebecca03@gmail.com>
2023-10-16 16:53:04 +0100

    updated the countdown div

--- main.js
+++ main.js
@@ -14,3 +14,29 @@ textCollapseDiv.addEventListener('click', e =>{
         // }
     }
 })
+
+
+// countdown logic
+var countDownDate = new Date("October 17, 2023 00:00:00").getTime();
+console.log(countDownDate)
+
+var x = setInterval(function() { 
+    var now = new Date().getTime();
+    var distance = countDownDate - now;
+
+    // var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  document.getElementById("hours").innerHTML = hours;
+  document.getElementById("minutes").innerHTML = minutes;
+  document.getElementById("seconds").innerHTML = seconds;
+
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("hours").innerHTML = "EXPIRED";
+    document.getElementById("minutes").innerHTML.style = "display:none";
+  document.getElementById("seconds").innerHTML.style= "display:none";
+  }
+}, 1000);

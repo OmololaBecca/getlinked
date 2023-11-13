a704b3112d08b9f77574697b5684f9127844290d
OmololaBecca <folarebecca03@gmail.com>
2023-11-13 13:06:39 +0100

    updating the mobile view

--- main.js
+++ main.js
@@ -3,42 +3,38 @@ const inputFieldOne= document.getElementById("input-one")
 const inputFieldTwo= document.getElementById("input-two")
 const inputFieldThree= document.getElementById("input-three")
 const inputFieldFour= document.getElementById("input-four")
+const mobileBar = document.getElementById("mobile-bar")
+const mobileNavPage =document.getElementById("mobile-nav")
+
+console.log(mobileBar)
 
 
+// mobile bar codes
 
-registerNowButton.addEventListener('click', ()=>{
-   if(inputFieldOne.value  == ""){
-    console.log('yes')
-  }else if (inputFieldTwo.value == ""){
-    console.log('yes')
-  }else if (inputFieldThree.value ==""){
-    console.log('yes')
-  }else if (inputFieldFour.value ==""){
-    console.log('yes')
-  }else{
-    console.log('woo')
-    window.open("confirmationpage.html")
-  }
-  
-  
-})
-
-
-
-const textCollapseDiv = document.querySelector(".text-collapse-div")
- console.log(textCollapseDiv.children)
-
-
-    textCollapseDiv.addEventListener('click', e =>{   
-        for(let collapsediv = 0; collapsediv < textCollapseDiv.children.length; collapsediv++){
-       let div = textCollapseDiv[collapsediv]
-console.log(e.target)
+function addToggle(){
+  mobileBar.classList.remove("fa-bars")
+  mobileBar.classList.add("fa-times")
+  mobileBar.classList.add("clicked")
+  mobileNavPage.style="display:block"
 }
-       
- })
+function removeToggle(){
+mobileBar.classList.add("fa-bars")
+mobileBar.classList.remove("fa-times")
+mobileBar.classList.remove("clicked")
 
+}
 
-
+mobileBar.addEventListener('click', ()=>{
+ const isClicked = mobileBar.classList.contains("clicked")
+ if(isClicked){
+   removeToggle()
+   console.log('hello')
+ }else{
+   addToggle()
+   console.log('who you be')
+ }
+console.log(mobileBar)
+})
 
 // countdown logic
 var countDownDate = new Date("December 26, 2023 00:00:00").getTime();
@@ -64,3 +60,43 @@ var x = setInterval(function() {
   document.getElementById("seconds").innerHTML.style= "display:none";
   }
 }, 1000);
+
+
+
+
+
+
+registerNowButton.addEventListener('click', ()=>{
+   if(inputFieldOne.value  == ""){
+    console.log('yes')
+  }else if (inputFieldTwo.value == ""){
+    console.log('yes')
+  }else if (inputFieldThree.value ==""){
+    console.log('yes')
+  }else if (inputFieldFour.value ==""){
+    console.log('yes')
+  }else{
+    console.log('woo')
+    window.open("confirmationpage.html")
+  }
+  
+})
+
+
+
+const textCollapseDiv = document.querySelector(".text-collapse-div")
+ console.log(textCollapseDiv.children)
+
+
+    textCollapseDiv.addEventListener('click', e =>{   
+        for(let collapsediv = 0; collapsediv < textCollapseDiv.children.length; collapsediv++){
+       let div = textCollapseDiv[collapsediv]
+console.log(e.target)
+}
+       
+ })
+
+
+
+
+
